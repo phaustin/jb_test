24fc7d4ab5640ae61b14713776f76a183d761c98
Phil Austin <paustin@eos.ubc.ca>
2020-07-08 11:27:56 -0700

    add commands

---
+++ doit.sh
@@ -0,0 +1,3 @@
+python runbook.py build --toc books/toc_urllink/_toc_urllink.yml books/toc_urllink
+
+python runbook.py build --toc books/toc_startswithheader/_toc_startswithheader.yml books/toc_startswithheader
